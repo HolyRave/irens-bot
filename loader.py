70181ddecd77ad38efeb491d3dc92899c7b89da7
HolyRave <ilya.balitskiy@stud.onu.edu.ua>
2021-05-01 21:21:36 +0300

    created Dockerfile

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.rethinkdb import RethinkDBStorage
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
+storage = RethinkDBStorage(db=config.DB_creds.get('db_name'), table='aiogram', user=config.DB_creds.get('username'),
+                           password=config.DB_creds.get('password'), host=config.DB_creds.get('host'),
+                           port=int(config.DB_creds.get('port')))
 dp = Dispatcher(bot, storage=storage)
